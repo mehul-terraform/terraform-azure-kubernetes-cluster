0c3daefbd56fd62c493aa7a69740e8c0c43b5837
Your Name <you@example.com>
2025-06-24 18:24:01 +0530

    CodeUpdated

--- Terraform/0-Provider.tf
+++ Terraform/0-Provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.1"
+      version = ">3.84.0"
     }
   }
 }
@@ -10,6 +10,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b"
+  #skip_provider_registration = true
   #  subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b" # "az account show --query id -o tsv"
 }
+
 
